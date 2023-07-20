27ad8b65786853fe7baf1e1cf7b72ab1ff72de85
xaletch <kiril.kolesnikov0609@mail.ru>
2023-07-20 16:12:21 +0300

    remove component CatalogProduct

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { CatalogProduct } from './CatalogProduct'
 import { ButtonComeBack } from './ButtonComeBack'
+import { ProductBlock } from './ProductBlock'
 
-export const Catalog = ({ products }) => {
+export const Catalog = ({ products,  }) => {
   return (
     <div className='catalog'>
         <div className='container'>
             <ButtonComeBack />
             <h2 className='cart__title'>Каталог</h2>
             <div className='home__best--offer__bottom flex'>
-                {products.map((obj: any ) => <CatalogProduct key={obj.id} {...obj}/> )}
+                {products.map((obj: any ) => <ProductBlock key={obj.id} {...obj}/> )}
             </div>
         </div>
     </div>
